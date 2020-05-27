2f3b23d779ca55e8631dfa3c3a46c99b65710b49
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-05-27 10:37:52 +0300

    updated style

--- src/main/java/com/cinema/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/com/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -10,7 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Root;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
