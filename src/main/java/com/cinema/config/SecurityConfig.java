04a904d590995389200f7661846021e84696cf24
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-12 14:05:50 +0300

    added Security config and initializer

---
+++ src/main/java/com/cinema/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.cinema.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
